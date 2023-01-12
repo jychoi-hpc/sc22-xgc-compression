49972d0e4bfbcbf7cf3d45e00bd19924baed606e
Choi <choij@ornl.gov>
2023-01-11 22:39:30 -0500

    add e_f

--- xgc_compress.cpp
+++ xgc_compress.cpp
@@ -21,21 +21,21 @@
 
 static void show_usage(std::string name)
 {
-    std::cerr << "Usage: " << name << " EXPDIR NP_PER_PLANE BSTEP ESTEP INC SLEEP_SEC [NNODES]" << std::endl;
+    std::cerr << "Usage: " << name << " EXPDIR" << std::endl;
 }
 
 int main(int argc, char *argv[])
 {
-    int rank, size;
+    int rank, comm_size;
     MPI_Comm comm = MPI_COMM_WORLD;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(comm, &rank);
-    MPI_Comm_size(comm, &size);
+    MPI_Comm_size(comm, &comm_size);
     if (rank == 0)
-        printf("rank,size: %d %d\n", rank, size);
+        printf("rank,comm_size: %d %d\n", rank, comm_size);
 
-    if (argc < 7)
+    if (argc < 2)
     {
         if (rank == 0)
             show_usage(argv[0]);
@@ -43,84 +43,22 @@ int main(int argc, char *argv[])
     }
 
     std::string expdir = argv[1];
-    int np_per_plane = atoi(argv[2]); // number of PEs per plane
-    // int bstep = atoi(argv[3]);        // start step index
-    // int estep = atoi(argv[4]);        // end step index
-    // int inc = atoi(argv[5]);          // inc
-    int sleep_sec = atoi(argv[6]);
-    char *accu = argv[7];
-    int ptype = atoi(argv[8]);
-    char *precision = argv[9];
-    char *train_yes = argv[10];
-    char *use_pre = argv[11];
-    char *use_ddp = argv[12];
-    char *epochs = argv[13];
-    char *ae_error = argv[14];
-    char *pqbits = argv[15];
-    char *lr = argv[16];
-    char *decomp = argv[17];
-    char *lbound = argv[18];
-    char *ubound = argv[19];
-    int user_nnodes = 0; // user defined nnodes (optional)
-    if (argc > 20)
-        user_nnodes = atoi(argv[20]);
 
     if (rank == 0)
-    {
         printf("expdir: %s\n", expdir.data());
-        printf("np_per_plane: %d\n", np_per_plane);
-        printf("sleep_sec: %d\n", sleep_sec);
-        printf("accuracy: %s\n", accu);
-        printf("species: %d\n", ptype);
-        printf("precision: %s\n", precision);
-        printf("train: %s\n", train_yes);
-        printf("use_pre: %s\n", use_pre);
-        printf("use_ddp: %s\n", use_ddp);
-        printf("epochs: %s\n", epochs);
-        printf("ae_error: %s\n", ae_error);
-        printf("pqbits: %s\n", pqbits);
-        printf("learning rate: %s\n", lr);
-        printf("decomposition: %s\n", decomp);
-        printf("lbound: %s\n", lbound);
-        printf("ubound: %s\n", ubound);
-        printf("user_nnodes: %d\n", user_nnodes);
-    }
     MPI_Barrier(comm);
-    long unsigned int nphi = 0;                   // number of planes (will be set after reading)
-    long unsigned int nplane_per_rank = 1;        // number of planes per rank
-    long unsigned int iphi = rank / np_per_plane; // plane index
-    int plane_rank = rank % np_per_plane;         // rank in plane
-    printf("%d: iphi, plane_rank:\t%d\t%d\n", rank, iphi, plane_rank);
-    MPI_Barrier(comm);
-
-    // Use yaml for extra parameters
-    std::map<std::string, std::string> params2;
-    try
-    {
-        YAML::Node config = YAML::LoadFile("config.yaml");
-        if (rank == 0)
-            std::cout << "Operation parameters:" << std::endl;
-        for (YAML::const_iterator it = config.begin(); it != config.end(); ++it)
-        {
-            if (rank == 0)
-                std::cout << " " << it->first.as<std::string>() << ": " << it->second.as<std::string>() << std::endl;
-            params2.insert({it->first.as<std::string>(), it->second.as<std::string>()});
-        }
-    }
-    catch (...)
-    {
-        // do nothing
-    }
+    long unsigned int nphi = 0;            // number of planes (will be set after reading)
+    long unsigned int nplane_per_rank = 0; // number of planes per rank
+    long unsigned int iphi = 0;            // plane index
+    int plane_rank = 0;                    // rank in plane
+    int np_per_plane = 0;
 
     adios2::ADIOS ad("adios2cfg.xml", comm);
     adios2::IO io = ad.DeclareIO("f0");
     adios2::IO wio = ad.DeclareIO("f0compress");
     adios2::Engine writer;
 
-    const char *varname = ptype == 1 ? "i_f" : "e_f";
-    const char *argname = ptype == 1 ? "ion" : "electron";
     char filename[50];
-    char meshfile[1024];
     long unsigned int nvp = 0;
     long unsigned int nnodes = 0;
     long unsigned int nmu = 0;
@@ -128,11 +66,34 @@ int main(int argc, char *argv[])
     long unsigned int l_offset = 0;
     static bool first = true;
 
+    // Use yaml to pass parameters to Adios operator
+    std::map<std::string, std::string> params;
+    try
+    {
+        YAML::Node root = YAML::LoadFile("params.yaml");
+        if (rank == 0)
+            std::cout << "Operation parameters:" << std::endl;
+        for (YAML::const_iterator it = root.begin(); it != root.end(); ++it)
+        {
+            if (rank == 0)
+                std::cout << " " << it->first.as<std::string>() << ": " << it->second.as<std::string>() << std::endl;
+            params.insert({it->first.as<std::string>(), it->second.as<std::string>()});
+        }
+    }
+    catch (...)
+    {
+        // do nothing
+    }
+
     sprintf(filename, "%s/restart_dir/xgc.f0.bp", expdir.data());
+    if (rank == 0)
+        printf("%d: Reading filename: %s\n", rank, filename);
     adios2::Engine reader = io.Open(filename, adios2::Mode::Read, comm);
+
     // for (int i = bstep; i < estep; i += inc)
     while (true)
     {
+        // Step #1: Read XGC data in step
         adios2::StepStatus read_status = reader.BeginStep();
         if (read_status != adios2::StepStatus::OK)
         {
@@ -141,59 +102,67 @@ int main(int argc, char *argv[])
 
         int istep = reader.CurrentStep();
 
-        // Step #1: Read original XGC data
-        if (rank == 0)
-            printf("%d: Reading filename: %s\n", rank, filename);
+        adios2::Variable<double> var_i_f = io.InquireVariable<double>("i_f");
+        adios2::Variable<double> var_e_f = io.InquireVariable<double>("e_f");
 
-        adios2::Variable<double> var_i_f = io.InquireVariable<double>(varname);
-
-        nphi = var_i_f.Shape()[0];
-        assert(("Wrong number of MPI processes.", size == nphi * np_per_plane));
-        nvp = var_i_f.Shape()[1];
-        nnodes = var_i_f.Shape()[2];
-        nmu = var_i_f.Shape()[3];
-
-        l_nnodes = nnodes / np_per_plane;
-        l_offset = plane_rank * l_nnodes;
-        if ((plane_rank % np_per_plane) == (np_per_plane - 1))
-            l_nnodes = l_nnodes + nnodes % np_per_plane;
-
-        // use user-defined nnodes if specified
-        if (user_nnodes > 0)
+        if (first)
         {
-            l_nnodes = user_nnodes;
-            l_offset = plane_rank * l_nnodes;
-            nnodes = size * user_nnodes;
-        }
-        int decomp_num = atoi(decomp);
-        if (decomp_num == 1 or decomp_num >= 5)
-        {                           //  == 0) {
-            nplane_per_rank = nphi; // number of planes per rank
-            iphi = 0;               // plane index
-            l_nnodes = nnodes / size;
-            l_offset = rank * l_nnodes;
-            if (rank == size - 1)
-            {
-                l_nnodes += nnodes % size;
-            }
-        }
-        printf("%d: Selection: (%d %d %d %d) (%d %d %d %d)\n", rank, iphi, 0, l_offset, 0, nplane_per_rank, nvp,
-               l_nnodes, nmu);
-        var_i_f.SetSelection({{iphi, 0, l_offset, 0}, {nplane_per_rank, nvp, l_nnodes, nmu}});
+            nphi = var_i_f.Shape()[0];        // number of planes
+            np_per_plane = comm_size / nphi;  // number of PEs per plane
+            nplane_per_rank = 1;              // number of planes per rank
+            iphi = rank / np_per_plane;       // plane index
+            plane_rank = rank % np_per_plane; // rank in plane
+            assert(("Wrong number of MPI processes.", comm_size == nphi * np_per_plane));
 
+            printf("%d: iphi, plane_rank:\t%d\t%d\n", rank, iphi, plane_rank);
+            MPI_Barrier(comm);
+
+            // Assume shape is not changing
+            nvp = var_i_f.Shape()[1];
+            nnodes = var_i_f.Shape()[2];
+            nmu = var_i_f.Shape()[3];
+
+            l_nnodes = nnodes / np_per_plane;
+            l_offset = plane_rank * l_nnodes;
+            if ((plane_rank % np_per_plane) == (np_per_plane - 1))
+                l_nnodes = l_nnodes + nnodes % np_per_plane;
+
+            int decomp_num = 0;
+            if (params.find("decomp") != params.end())
+                decomp_num = atoi(params["decomp"].c_str());
+            if (decomp_num == 1 or decomp_num >= 5)
+            {                           //  == 0) {
+                nplane_per_rank = nphi; // number of planes per rank
+                iphi = 0;               // plane index
+                l_nnodes = nnodes / comm_size;
+                l_offset = rank * l_nnodes;
+                if (rank == comm_size - 1)
+                {
+                    l_nnodes += nnodes % comm_size;
+                }
+            }
+            printf("%d: Selection: (%d %d %d %d) (%d %d %d %d)\n", rank, iphi, 0, l_offset, 0, nplane_per_rank, nvp,
+                   l_nnodes, nmu);
+            var_i_f.SetSelection({{iphi, 0, l_offset, 0}, {nplane_per_rank, nvp, l_nnodes, nmu}});
+        }
+
+        if (rank == 0)
+            printf("%d: Reading step: %d\n", rank, istep);
         std::vector<double> i_f;
+        std::vector<double> e_f;
         reader.Get<double>(var_i_f, i_f);
-        // End adios2 step
+        reader.Get<double>(var_e_f, e_f);
+        // End of adios2 read step
         reader.EndStep();
 
         // Step #2: Simulate computation
-        int interval = 10;
-        for (int k = sleep_sec; k > 0; k = k - interval)
-        {
-            if (rank == 0)
-                printf("%d: Computation: %d seconds left.\n", rank, k);
-            std::this_thread::sleep_for(std::chrono::seconds(MIN(k, interval)));
-        }
+        // int interval = 10;
+        // for (int k = sleep_sec; k > 0; k = k - interval)
+        // {
+        //     if (rank == 0)
+        //         printf("%d: Computation: %d seconds left.\n", rank, k);
+        //     std::this_thread::sleep_for(std::chrono::seconds(MIN(k, interval)));
+        // }
 
         // Step #3: Write f-data
         if (rank == 0)
@@ -202,40 +171,48 @@ int main(int argc, char *argv[])
         sprintf(output_fname, "xgc.f0_%d.bp", np_per_plane);
         if (first)
         {
-            auto var = wio.DefineVariable<double>(varname, {nphi, nvp, nnodes, nmu}, {iphi, 0, l_offset, 0},
-                                                  {nplane_per_rank, nvp, l_nnodes, nmu});
+            auto var_i_f = wio.DefineVariable<double>("i_f", {nphi, nvp, nnodes, nmu}, {iphi, 0, l_offset, 0},
+                                                      {nplane_per_rank, nvp, l_nnodes, nmu});
+            auto var_e_f = wio.DefineVariable<double>("e_f", {nphi, nvp, nnodes, nmu}, {iphi, 0, l_offset, 0},
+                                                      {nplane_per_rank, nvp, l_nnodes, nmu});
             // make params
-            sprintf(meshfile, "%s/xgc.f0.mesh.bp", expdir.data());
-            std::map<std::string, std::string> params = {{"tolerance", accu},
-                                                         {"mode", "ABS"},
-                                                         {"s", "0"},
-                                                         {"meshfile", meshfile},
-                                                         {"compression_method", "3"},
-                                                         {"pq", "0"},
-                                                         {"prec", precision},
-                                                         {"latent_dim", "4"},
-                                                         {"train", train_yes},
-                                                         {"species", argname},
-                                                         {"use_ddp", use_ddp},
-                                                         {"use_pretrain", use_pre},
-                                                         {"nepoch", epochs},
-                                                         {"ae_thresh", ae_error},
-                                                         {"pqbits", pqbits},
-                                                         {"lr", lr},
-                                                         {"leb", lbound},
-                                                         {"ueb", ubound},
-                                                         {"decomp", decomp}};
-            params.insert(params2.begin(), params2.end());
+            // sprintf(meshfile, "%s/xgc.f0.mesh.bp", expdir.data());
+            // std::map<std::string, std::string> params = {{"tolerance", accu},
+            //                                              {"mode", "ABS"},
+            //                                              {"s", "0"},
+            //                                              {"meshfile", meshfile},
+            //                                              {"compression_method",
+            //                                              "3"},
+            //                                              {"pq", "0"},
+            //                                              {"prec", precision},
+            //                                              {"latent_dim", "4"},
+            //                                              {"train", train_yes},
+            //                                              {"species", argname},
+            //                                              {"use_ddp", use_ddp},
+            //                                              {"use_pretrain", use_pre},
+            //                                              {"nepoch", epochs},
+            //                                              {"ae_thresh", ae_error},
+            //                                              {"pqbits", pqbits},
+            //                                              {"lr", lr},
+            //                                              {"leb", lbound},
+            //                                              {"ueb", ubound},
+            //                                              {"decomp", decomp}};
+            std::map<std::string, std::string> extra = {{"species", "ion"}};
+            params.insert(extra.begin(), extra.end());
 
             // add operator
-            var.AddOperation("mgardplus", params);
+            var_i_f.AddOperation("mgardplus", params);
+            params["species"] = "electron";
+            var_e_f.AddOperation("mgardplus", params);
             writer = wio.Open(output_fname, adios2::Mode::Write, comm);
             first = false;
         }
 
         writer.BeginStep();
-        auto var = wio.InquireVariable<double>(varname);
-        writer.Put<double>(var, i_f.data());
+        auto var_i_f2 = wio.InquireVariable<double>("i_f");
+        auto var_e_f2 = wio.InquireVariable<double>("e_f");
+        writer.Put<double>(var_i_f2, i_f.data());
+        writer.Put<double>(var_e_f2, e_f.data());
         writer.EndStep();
 
         if (rank == 0)
